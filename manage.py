85162594e532364e7a18d406b9a38dc5b2879ac6
dpitroda <pitroda.darshana@gmail.com>
2019-04-18 12:53:38 +0930

    test commmit

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import os
 import sys
 
@@ -6,5 +7,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CDUCourseSupportSystem.settings")
 
     from django.core.management import execute_from_command_line
-
+#sorry this is jsut xample. delete it later.
     execute_from_command_line(sys.argv)
