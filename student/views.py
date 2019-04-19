8dbc731a6165310d2cfa918aec9417d6d59a9cb1
dpitroda <pitroda.darshana@gmail.com>
2019-04-19 11:23:35 +0930

    Basic student mode created

--- student/views.py
+++ student/views.py
@@ -3,6 +3,10 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h2>Hello, Students<h2>")
+    #template = loader.get_template('student/index.html')
+    context = 0
+    return render(request, 'student/index.html', context)
